5f92ebc342c497da888c7bbb94388ea529f97070
ilayaraja97 <ilayaraja97@gmail.com>
2017-01-20 10:54:36 +0000

    finished lab1+bug fixes

--- SortContentDir/main.c
+++ SortContentDir/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argv,char *args[])
 {
-    freopen("input.txt","r",stdin);
+    //freopen("input.txt","r",stdin);
     //freopen("lsresult.txt","w",stdout);
     if(argv!=2)
     {
@@ -12,22 +13,26 @@ int main(int argv,char *args[])
     char cmd[1024];
     sprintf(cmd,"ls $HOME/%s>lsresult.txt",args[1]);
     system(cmd);
-    freopen("lsresult.txt","r",stdin);
+    FILE *fp=fopen("lsresult.txt","r");
+    if(feof(fp))
+    printf("something\'s bad\n");
     //freopen("output.txt","wa",stdout);
     sprintf(cmd,"mkdir \"$HOME/%s Sorted\"",args[1]);
     system(cmd);
-    char filename[1024];
+    char filename[1024],f1[1024]="______";
     while(1)
     {
-        if(!scanf("%s",filename))
+        fscanf(fp," %[^\n]s",filename);
+        if(strcmp(f1,filename)==0)
             break;
-        printf("%s",filename);
+        //printf("%s\n",filename);
         int ext=strlen(filename)-4;//a
-        if(strcmp(".txt",filename+ext)==0)
+        if(strcmp(".txt",filename+ext)==0||strcmp(".dat",filename+ext)==0)
         {
             sprintf(cmd,"sort \"$HOME/%s/%s\" -o \"$HOME/%s Sorted/%s\"",args[1],filename,args[1],filename);
             system(cmd);
         }
+        strcpy(f1,filename);
    }
     return 0;
 }
